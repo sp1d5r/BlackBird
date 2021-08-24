165fc07bf7ddbe3ff6ca7b1e4705a67fe638c7f7
sp1d5r <aa1719@imperial.ac.uk>
2021-08-24 18:41:22 +0100

    Initial Commit

---
+++ Blackbird/BlackbirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackbirdApp.swift
+//  Blackbird
+//
+//  Created by Almaz Ahmad on 24/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct BlackbirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
